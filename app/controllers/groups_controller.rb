e09f0c4527404916321ed2a65243d64b86ab8651
sdlong <sdlong.jeng@gmail.com>
2014-09-01 05:44:35 +0800

    4.2 - 使用者功能帶進 group 與 post 裡 ( 作者機制 )

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,11 +18,11 @@ class GroupsController < ApplicationController
 
 
   def edit
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
   
     if @group.save 
       redirect_to groups_path, :notice => '新增討論版成功'
@@ -32,7 +32,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
   
     if @group.update(group_params)
       redirect_to groups_path, :notice => '修改討論版成功'
@@ -42,7 +42,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
     
     @group.destroy
     redirect_to groups_path, :alert => '討論版已刪除'
