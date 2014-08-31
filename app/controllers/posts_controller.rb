db0fbe7a16759b17bf2e1df1692c49feccfa8333
sdlong <sdlong.jeng@gmail.com>
2014-08-31 21:28:49 +0800

    3.2 - 以 before_action 整理重複的程式碼

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,45 +1,42 @@
 class PostsController < ApplicationController
 
+  before_action :find_group
+
   def new
-    @group = Group.find(params[:group_id])
     @post = @group.posts.new
   end
 
   def edit
-    @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @post = @group.posts.new(post_params)
   
     if @post.save
-      redirect_to group_path(@group), :notice => '新增文章成功！'
+      redirect_to group_path(@group)
     else
       render :new
     end
   end 
 
   def update
-    @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to group_path(@group), :notice => '文章修改成功！'      
+      redirect_to group_path(@group), :notice => '文章修改成功！'
     else
       render :edit
     end
   end
 
   def destroy
-    @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
 
     @post.destroy
+
     redirect_to group_path(@group), :alert => '文章已刪除'
   end
-
 
 
   private
@@ -48,4 +45,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
 
+  def find_group
+      @group = Group.find(params[:group_id])
+  end
 end
